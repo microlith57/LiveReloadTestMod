21ab2db26ac68705840166e6f46e8b04f40c740c
Microlith57 <microlith57@gmail.com>
2022-03-26 22:41:55 +1300

    first commit

---
+++ src/LiveReloadTestModModule.cs
@@ -0,0 +1,29 @@
+﻿namespace Celeste.Mod.LiveReloadTestMod {
+    public class LiveReloadTestModModule : EverestModule {
+
+        public static LiveReloadTestModModule Instance;
+
+        public LiveReloadTestModModule() {
+            Instance = this;
+        }
+
+        public override void Load() {
+            On.Celeste.Player.DashBegin += OnDashBegin;
+        }
+
+        public override void Unload() {
+            On.Celeste.Player.DashBegin -= OnDashBegin;
+        }
+
+        private void OnDashBegin(On.Celeste.Player.orig_DashBegin orig, Player self) {
+            orig(self);
+
+
+            /* // from:
+            self.ExplodeLaunch(self.Center - self.DashDir, snapUp: true);
+            */ // to:
+            self.ExplodeLaunch(self.Center - (self.DashDir * 100), snapUp: true);
+        }
+
+    }
+}
